7a1a93845761e3dba9e1ff72e748a3667fc0b5a9
Jonathan Williams <u0637815@utah.edu>
2014-06-01 19:34:31 +0000

    added my uID to the text message thing

--- site.rb
+++ site.rb
@@ -10,7 +10,7 @@
  end
 
  post '/' do
-   RestClient.post 'https://AC01983fd2966d4f4b28a2624b8b8abfa7:a4fd823e75768b238b222b5c19f545fa@api.twilio.com/2010-04-01/Accounts/AC01983fd2966d4f4b28a2624b8b8abfa7/SMS/Messages/', :From => '+18017846920', :To => "#{params["sms"]}", :Body => 'Hello, from u1234567'
+   RestClient.post 'https://AC01983fd2966d4f4b28a2624b8b8abfa7:a4fd823e75768b238b222b5c19f545fa@api.twilio.com/2010-04-01/Accounts/AC01983fd2966d4f4b28a2624b8b8abfa7/SMS/Messages/', :From => '+18017846920', :To => "#{params["sms"]}", :Body => 'Hello, from u0637815'
   erb :thanks
  end
 
