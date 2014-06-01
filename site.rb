c4abf63b7ec3ffbf6e32ce5dadb97e7e9f805f23
Jonathan Williams <u0637815@utah.edu>
2014-06-01 19:09:11 +0000

    added final code

--- site.rb
+++ site.rb
@@ -1,10 +1,20 @@
  require 'sinatra'
+ require 'rest-client'
+
  ENV['PORT'] ||= '4000'
  set :port, ENV['PORT']
  set :bind, '0.0.0.0'
+
  get '/' do
    erb :index
  end
+
+ post '/' do
+   RestClient.post 'https://AC01983fd2966d4f4b28a2624b8b8abfa7:a4fd823e75768b238b222b5c19f545fa@api.twilio.com/2010-04-01/Accounts/AC01983fd2966d4f4b28a2624b8b8abfa7/SMS/Messages/', :From => '+18017846920', :To => "#{params["sms"]}", :Body => 'Hello, from u1234567'
+  erb :thanks
+ end
+
+
  get '/italian' do
    "Ciao!"
  end
